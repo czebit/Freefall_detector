1ea07f63d32a86a7ecdd835b30642b70def14580
wnowosiad <w.nowosiad@gmail.com>
2019-12-16 16:35:24 +0100

    trying sth with i2c

--- i2c.c
+++ i2c.c
@@ -9,4 +9,12 @@ void i2cInit(void){
 	PORTB->PCR[SCL_PIN] |= PORT_PCR_MUX(2); // using PTB3 as i2c_scl
 	PORTB->PCR[SDA_PIN] |= PORT_PCR_MUX(2); // using PTB4 as i2c_sda
 	
+	I2C0->F  = 0x14;
+	I2C0->C1 = I2C_C1_IICEN_MASK;
+	
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;       // Turn on clock to Port A module
+	PORTA->PCR[24] |= (0|PORT_PCR_ISF_MASK|     // Clear the interrupt flag
+		PORT_PCR_MUX(0x1)|     // PTA14 is configured as GPIO
+		PORT_PCR_IRQC(0xA));   // PTA14 is configured for falling edge interrupts
+	
 }
